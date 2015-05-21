b19f4701a41cf235ed7299d668285c484d1b134f
Parth Thakkar <thakkarparth007@gmail.com>
2015-05-21 23:09:29 +0530

    Sys Ad Task 1 completed

--- DeltaSysAd/task1.sh
+++ DeltaSysAd/task1.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-cd $1
-mkdir ./folder{1..100}
-for i in {1..10}; do
-	touch "./folder"$i"/folder"$i".txt"
-	chmod 600 "./folder"$i"/folder"$i".txt"
-done
+if [ "$#" -ne 1 ]; then
+cat <<-EOF
+	*******************************************************
+	Delta Task 1:
+	*******************************************************
+	This script expects exactly 1 parameter - the directory
+	in which the 100 folders must be created. Note that the 
+	script will give an error in case the directory given
+	as the parameter doesn't exist. You must create the directory
+	first.
+	EOF
+else
+	cd $1
+	mkdir ./folder{1..100}
+	for i in {1..10}; do
+		touch "./folder"$i"/folder"$i".txt"
+		chmod 700 "./folder"$i"/folder"$i".txt"
+	done
+fi
